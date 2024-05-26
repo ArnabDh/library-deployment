ecc98d4ff6e71b8e3f800bd0c460eece18aa04ed
ArnabDh <arnabdhara2022@gmail.com>
2024-05-26 16:44:32 +0530

    sixth commit

--- static/main.js
+++ static/main.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://0.0.0.0:8000';
+const BASE_URL = window.BASE_URL;
 let accessToken='';
 async function signup() {
     const username = document.getElementById('signup-username').value;
